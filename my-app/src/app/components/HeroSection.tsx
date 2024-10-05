c299dbd294f4bc1d75ad10cb422e708d0220f672
Shivesh Nandan <kshivesh929@gmail.com>
2024-10-05 08:17:33 +0530

    few changes

--- my-app/src/app/components/HeroSection.tsx
+++ my-app/src/app/components/HeroSection.tsx
@@ -4,21 +4,29 @@ import React, { useState } from "react";
 const HeroSection = () => {
   const [modal, setmodal] = useState(false);
   const [create, setcreate] = useState(true);
+  const [value, setValue] = useState<number>(0);
+
+  const handleIncrement = () => {
+    setValue(value + 100);
+  };
+  const handleDecrement = () => {
+    setValue(Math.max(value - 100,0));
+  };
   return (
-    <div className="bg-[url('../../public/bgImg.png')] w-full h-[100vh] bg-center bg-cover ">
-      <div className="pt-20 w-10/12 m-auto flex justify-around h-screen">
-        <div className="bg-[#ffffff60] my-auto rounded-xl text-center shadow-lg">
-          <img src="multi.png" alt="" className="w-[220px] mx-16 my-5" />
+    <div className="bg-[url('../../public/bgImg.png')] w-full h-[100vh] bg-center bg-cover max-md:h-[130vh]">
+      <div className="pt-20 w-10/12 m-auto flex justify-around h-screen max-md:flex-col ">
+        <div className="bg-[#ffffff60] my-auto rounded-xl text-center shadow-lg w-[350px] max-md:w-[300px] max-md:mx-auto max-md:mt-10">
+          <img src="multi.png" alt="" className="w-[220px] mx-auto my-5 max-md:w-[200px]" />
           <button
             onClick={() => setmodal(true)}
-            className="bg-black m-auto flex mb-8 py-3 justify-center rounded-xl w-10/12 uppercase font-semibold text-2xl "
+            className="bg-black m-auto flex mb-8 py-3 justify-center rounded-xl w-10/12 uppercase font-semibold text-2xl max-md:text-xl"
           >
             Play with Friends
           </button>
         </div>
-        <div className="bg-[#ffffff60] my-auto rounded-xl text-center shadow-lg">
-          <img src="solo.png" alt="" className="w-[220px] mx-16 my-5" />
-          <button className="bg-black m-auto flex mb-8 py-3 justify-center rounded-xl w-10/12 uppercase font-semibold text-2xl ">
+        <div className="bg-[#ffffff60] my-auto rounded-xl text-center shadow-lg w-[350px] max-md:w-[300px] max-md:mx-auto max-md:my-10 ">
+          <img src="solo.png" alt="" className="w-[220px] mx-auto my-5 max-md:w-[180px]" />
+          <button className="bg-black m-auto flex mb-8 py-3 justify-center rounded-xl w-10/12 uppercase font-semibold text-2xl mx-auto max-md:text-xl">
             Play solo
           </button>
         </div>
@@ -43,7 +51,7 @@ const HeroSection = () => {
                 >
                   Create
                 </p>
-                <p className={`${create ? "text-[#6D6969]" : "font-[700]"}`}>
+                <p onClick={() => setcreate(false)} className={`${create ? "text-[#6D6969]" : "font-[700]"} cursor-pointer`}>
                   Join
                 </p>
               </div>
@@ -51,17 +59,18 @@ const HeroSection = () => {
               <div className={`${create ? "" : "hidden"}`}>
                 <p className={`text-center py-8 text-2xl`}>select lobby fee</p>
                 <div className={`flex justify-around py-6`}>
-                  <img src="Minus.png" alt="" className="my-auto w-[40px]" />
+                  <img onClick={()=> handleDecrement()} src="Minus.png" alt="" className="my-auto w-[40px] cursor-pointer" />
                   <img src="diamond.png" alt="" className="w-[160px]" />
-                  <img src="Plus.png" alt="" className="my-auto w-[40px]" />
+                  <img onClick={()=>handleIncrement()} src="Plus.png" alt="" className="my-auto w-[40px] cursor-pointer" />
                 </div>
                 <input
                   type="number"
+                  value={value}
                   className="w-1/2 m-auto flex text-center rounded py-1 text-black px-5"
                 />
               </div>
               <div className={`${!create ? "" : "hidden"}`}>
-                <p className={`text-center py-8 text-2xl`}>select lobby fee</p>
+                <p className={`text-center py-8 text-2xl`}>enter lobby code</p>
                 <input
                   type="text"
                   className="w-1/2 m-auto flex text-center rounded py-1 text-black px-5"
@@ -71,7 +80,7 @@ const HeroSection = () => {
 
             <div className="flex flex-col m-3 ">
               <button
-                onClick={() => setcreate(false)}
+                onClick={() => setmodal(false)}
                 className="py-1 mt-2 w-1/2 m-auto bg-[#342F2C] uppercase border border-white rounded-lg"
               >
                 Next
